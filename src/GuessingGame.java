67a6635c35bdee80487edcc8a127d1523eaeee1f
kcdissanayaka <kc.dissanayaka@outlook.com>
2023-09-24 12:14:35 +0300

    CodeModifiedtoGetSuccessrates

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -6,6 +6,7 @@ public class GuessingGame {
 		Scanner console = new Scanner(System.in);
 		String userInput ;
 		int attempts = 0;
+		int success = 0;
 		
 		//System.out.println("Guess my name (type stop to exit)");
 		//console.nextLine().toLowerCase();
@@ -17,13 +18,25 @@ public class GuessingGame {
 			System.out.println("Guess my name (type stop to exit)");
 			userInput = console.nextLine();
 			if (userInput.contentEquals("Olivia")||userInput.contentEquals("Ava")
-					||userInput.contentEquals("Emma")||!userInput.contentEquals("stop")) 
+					||userInput.contentEquals("Emma")) 
 			{
 				attempts +=1;
-				System.out.println(attempts);
+				success +=1; 
 				
-			}
-			
+				if(success == 3) 
+				{	
+					
+					System.out.println("Congratulations!"+ "\n"+"You guessed 3 times.");
+				}
+				
+			else if (!userInput.contentEquals("stop")) 
+			{
+					
+				attempts +=1;
+			}	
+								
+			} 
+						
 		} while (!userInput.equals("stop")); 
 		
 
